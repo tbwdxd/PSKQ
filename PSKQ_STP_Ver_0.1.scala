8b903c59068341c6b7c694c1d16cd3078401940d
cloudera <cloudera@Master.Hadoop>
2016-07-22 01:40:01 -0700

    use sbt to all the dependencies

--- PSKQ_STP_Ver_0.1.scala
+++ PSKQ_STP_Ver_0.1.scala
@@ -7,7 +7,7 @@ Usage: scalac PSKQ_STP_ver_0.1.scala >> scala PSKQ
 */
 
 //Need to use the java json library
-import org.json.*;
-
-
+//import org.json.*;
+import org.json4s._ 
+import org.json4s.native.JsonMethods._ 
 //actuall code
---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "PSKQ"
+
+version :=  "0.1"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+	"org.json4s" %% "json4s-native" % "3.4.0"
+)
