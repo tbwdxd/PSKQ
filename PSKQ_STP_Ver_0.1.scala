b1da2bdd758c856a4611ece3fbeae2759d645170
cloudera <cloudera@Master.Hadoop>
2016-07-22 16:23:27 -0700

    now we can use sbt compile to compile the code, but the code has some problems.

--- PSKQ_STP_Ver_0.1.scala
+++ PSKQ_STP_Ver_0.1.scala
@@ -11,3 +11,33 @@ Usage: scalac PSKQ_STP_ver_0.1.scala >> scala PSKQ
 import org.json4s._ 
 import org.json4s.native.JsonMethods._ 
 //actuall code
+
+/*
+Generator Filter
+Keyword Match Processor
+Popularity Evaluator
+Spatial Constraint Processor
+PSK Randk Evaluator
+*/
+
+import org.apache.spark.graphx._
+// Import random graph generation library
+import org.apache.spark.graphx.util.GraphGenerators
+// A graph with edge attributes containing distances
+val graph: Graph[Int, Double] =
+  GraphGenerators.logNormalGraph(sc, numVertices = 100).mapEdges(e => e.attr.toDouble)
+val sourceId: VertexId = 42 // The ultimate source
+// Initialize the graph such that all vertices except the root have distance infinity.
+val initialGraph = graph.mapVertices((id, _) => if (id == sourceId) 0.0 else Double.PositiveInfinity)
+val sssp = initialGraph.pregel(Double.PositiveInfinity)(
+  (id, dist, newDist) => math.min(dist, newDist), // Vertex Program
+  triplet => {  // Send Message
+    if (triplet.srcAttr + triplet.attr < triplet.dstAttr) {
+      Iterator((triplet.dstId, triplet.srcAttr + triplet.attr))
+    } else {
+      Iterator.empty
+    }
+  },
+  (a,b) => math.min(a,b) // Merge Message
+  )
+println(sssp.vertices.collect.mkString("\n"))
--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ version :=  "0.1"
 scalaVersion := "2.11.7"
 
 libraryDependencies ++= Seq(
-	"org.json4s" %% "json4s-native" % "3.4.0"
+	"org.apache.spark" %% "spark-core" % "1.6.2",
+	"org.json4s" %% "json4s-native" % "3.4.0",
+	"org.apache.spark" % "spark-graphx_2.10" % "0.9.0-incubating"
 )
