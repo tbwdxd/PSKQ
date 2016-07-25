7cfa2e83f99c752e3e0d9a795001dba8e2ddff52
cloudera <cloudera@Master.Hadoop>
2016-07-25 14:44:20 -0700

    GG

--- PSKQ_STP_Ver_0.1.scala
+++ PSKQ_STP_Ver_0.1.scala
@@ -7,13 +7,6 @@ Usage: scalac PSKQ_STP_ver_0.1.scala >> scala PSKQ
 */
 package PSKQ
 
-
-//Need to use the java json library
-//import org.json.*;
-import org.json4s._ 
-import org.json4s.native.JsonMethods._ 
-//actuall code
-
 /*
 Generator Filter
 Keyword Match Processor
@@ -22,12 +15,42 @@ Spatial Constraint Processor
 PSK Randk Evaluator
 */
 
+object PSKQ_shortest_path extends App {
+  //Need to use the java json library
+  //import org.json.*;
+  import org.json4s._ 
+  import org.json4s.JsonDSL._
+  import org.json4s.jackson.JsonMethods._
+  //actuall code
+
+  def main{
+    case class Winner(id: Long, numbers: List[Int])
+    case class Lotto(id: Long, winningNumbers: List[Int], winners: List[Winner], drawDate: Option[java.util.Date])
+
+    val winners = List(Winner(23, List(2, 45, 34, 23, 3, 5)), Winner(54, List(52, 3, 12, 11, 18, 22)))
+    val lotto = Lotto(5, List(2, 45, 34, 23, 7, 5, 3), winners, None)
+
+    val json =
+      ("lotto" ->
+      ("lotto-id" -> lotto.id) ~
+      ("winning-numbers" -> lotto.winningNumbers) ~
+      ("draw-date" -> lotto.drawDate.map(_.toString)) ~
+      ("winners" ->
+        lotto.winners.map { w =>
+          (("winner-id" -> w.id) ~
+           ("numbers" -> w.numbers))}))
+
+    println(compact(render(json)))
+  }
+}
 
 //TODO
-object PSKQ_shortest_path{
-  def json parser(){
+/*object {
+  /*val edges1 List = ();
+  val verticies1 List= ();
+  val edges2 List= ();
+  val verticies2 List= ();*/
 
-  }
 
-
-}
+  def load_json();
+}*/
